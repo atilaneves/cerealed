c70306a7e3a33b2670dac2d4a55d06c299c22139
Atila Neves <aalvesne@cisco.com>
2013-08-01 19:50:40 +0200

    Added UByte decode tests

--- tests/decode.d
+++ tests/decode.d
@@ -11,6 +11,7 @@ void testDecodeBool() {
     cereal.grain(val); checkEqual(val, false);
     cereal.grain(val); checkEqual(val, false);
     cereal.grain(val); checkEqual(val, true);
+    checkThrown!RangeError(cereal.value!bool); //no more bytes
 }
 
 void testDecodeByte() {
@@ -18,5 +19,14 @@ void testDecodeByte() {
     checkEqual(cereal.value!byte, 0);
     checkEqual(cereal.value!byte, 2);
     checkEqual(cereal.value!byte, -4);
+    checkThrown!RangeError(cereal.value!byte); //no more bytes
+}
+
+
+void testDecodeUByte() {
+    auto cereal = new Decerealiser([0x0, 0x2, 0xfc]);
+    checkEqual(cereal.value!ubyte, 0);
+    checkEqual(cereal.value!ubyte, 2);
+    checkEqual(cereal.value!ubyte, 252);
     checkThrown!RangeError(cereal.value!byte); //no more bytes
 }
