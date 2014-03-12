abc610d328be560e1f20763ef7233d88caff705e
Atila Neves <aalvesne@cisco.com>
2014-03-12 18:15:37 +0100

    Added .dir-locals.el so that flycheck includes the necessary dirs

---
+++ tests/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((d-mode
+  (flycheck-dmd-include-path . ("~/.dub/packages/unit-threaded-master" ".."))))
