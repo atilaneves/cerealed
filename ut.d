7e640ae1db4e438de2f4f36fcd9b56b1c2b87746
Atila Neves <aalvesne@cisco.com>
2015-03-26 12:05:44 +0000

    Refactored ut.d

--- ut.d
+++ ut.d
@@ -4,7 +4,22 @@ import std.stdio;
 
 int main(string[] args) {
     writeln("\nAutomatically generated file ut.d");
-    writeln(`Running unit tests from dirs ["tests"]
-`);
-    return runTests!("tests.cerealiser_impl", "tests.reset", "tests.enums", "tests.decode", "tests.pointers", "tests.encode", "tests.encode_decode", "tests.bugs", "tests.structs", "tests.classes", "tests.range", "tests.compile_time")(args);
+    writeln(`Running unit tests from dirs ["tests"]`);
+    return args.runTests!(
+                          "tests.cerealiser_impl",
+                          "tests.multidimensional_array",
+                          "tests.reset",
+                          "tests.nested",
+                          "tests.enums",
+                          "tests.decode",
+                          "tests.static_array",
+                          "tests.pointers",
+                          "tests.encode",
+                          "tests.encode_decode",
+                          "tests.bugs",
+                          "tests.structs",
+                          "tests.classes",
+                          "tests.range",
+                          "tests.compile_time"
+                          );
 }
