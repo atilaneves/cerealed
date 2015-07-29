48b77457c2e46c8e8780411c87c26189077c4e93
Atila Neves <aalvesne@cisco.com>
2015-07-29 11:08:42 +0200

    Deleted .dir-locals.el, no longer needed

--- tests/.dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; See Info node `(emacs) Directory Variables' for more information.
-
-((d-mode
-  (flycheck-dmd-include-path . ("~/.dub/packages/unit-threaded-master/source" ".."))))
