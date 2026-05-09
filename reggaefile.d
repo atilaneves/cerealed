3340ea8b1881588c30f7f363f3dec0d5d13ec372
Atila Neves <atila.neves@gmail.com>
2026-05-09 10:48:37 +0200

    Update reggaefile

--- reggaefile.d
+++ reggaefile.d
@@ -1,7 +1,3 @@
 import reggae;
-
-alias ut = dubTestTarget!(CompilerFlags("-w -g -debug"),
-                          LinkerFlags());
-alias utl = dubConfigurationTarget!(Configuration("ut"),
-                                    CompilerFlags("-w -g -debug -unittest -version=unitThreadedLight"));
-mixin build!(ut, utl);
+alias ut = dubTest!();
+mixin build!(ut);
